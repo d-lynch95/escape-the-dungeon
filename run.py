02687372e72aa9f4ebd7a5e9e58d6727fac9c1c6
d-lynch95 <darraghlynch1995@gmail.com>
2023-04-07 12:43:49 +0000

    add test print statement

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import time
+import random
+import os
+
+a = 2
+b = 3
+c = 4
+
+
+print("hello world")
+
